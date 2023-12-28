ef937c253043e7f35f44ce88f7a9e94e2641d324
Ryland Birchmeier <rylandbirchmeier@gmail.com>
2023-12-27 21:06:50 -0500

    working on diffusers

--- src/main.py
+++ src/main.py
@@ -2,9 +2,32 @@ from config import *
 
 
 def main():
-    raw_markdown = "## Introduction\n\nIn the world of modern web development, interacting with APIs (Application Programming Interfaces) is a fundamental skill. Python, being a versatile and powerful language, provides numerous libraries and tools for making API requests. Whether you are a beginner or an experienced developer, mastering the art of writing API requests in Python can greatly enhance your ability to fetch data, integrate with third-party services, and build robust applications.\n\nBefore diving into the details of how to write API requests in Python, it's essential to understand the basics of APIs and HTTP protocols. An API acts as a bridge that allows different software applications to communicate with each other. The HTTP protocol, on the other hand, serves as the foundation for data communication on the World Wide Web.\n\n### Getting Started with API Requests\n\n#### Using the requests Library\n\nOne of the most popular and straightforward ways to make API requests in Python is by using the `requests` library. This library provides a simple and elegant way to send HTTP requests and handle the corresponding responses. To get started, make sure to install the `requests` library using pip:\n\n```bash\n$ pip install requests\n```\n\nOnce the library is installed, you can begin by making simple GET requests to retrieve data from a given API endpoint. Here's a basic example of how to make a GET request using the `requests` library:\n\n```python\nimport requests\n\nresponse = requests.get('https://api.example.com/data')\nprint(response.json())\n```\n\nIn this example, we use the `get` method of the `requests` module to fetch data from the specified URL. The `response.json()` method is then used to extract and parse the JSON data returned by the API.\n\n#### Handling Authentication and Parameters\n\nMany APIs require authentication or additional parameters to be included in the request. In Python, you can handle these requirements using the `requests` library as well. For example, if an API requires an API key for authentication, you can include it in the request using the `params` parameter:\n\n```python\nimport requests\n\napi_key = 'your_api_key_here'\nparameters = {'key': api_key, 'query': 'search_term'}\nresponse = requests.get('https://api.example.com/search', params=parameters)\nprint(response.json())\n```\n\n### Advanced API Request Handling\n\n#### Dealing with POST, PUT, and DELETE Requests\n\nIn addition to making GET requests, you may need to handle other HTTP methods such as POST, PUT, and DELETE when working with APIs. The `requests` library provides intuitive methods to handle these operations. Here's an example of making a POST request using `requests`:\n\n```python\nimport requests\n\npayload = {'key1': 'value1', 'key2': 'value2'}\nresponse = requests.post('https://api.example.com/create', data=payload)\nprint(response.json())\n```\n\nSimilarly, you can use the `put` and `delete` methods for handling PUT and DELETE requests, respectively.\n\n#### Error Handling and Response Codes\n\nWhen interacting with APIs, it's crucial to handle error responses and understand the meaning of different HTTP status codes. The `requests` library makes it easy to access the status code and handle potential errors in your API requests. Here's an example of checking the status code and handling potential errors using `requests`:\n\n```python\nimport requests\n\nresponse = requests.get('https://api.example.com/data')\nif response.status_code == 200:\n    print('Request successful')\nelse:\n    print(f'Request failed with status code: {response.status_code}')\n```\n\n### Conclusion\n\nMastering the art of writing API requests in Python is an essential skill for any developer looking to build robust and interconnected applications. With the `requests` library and the knowledge of HTTP methods and status codes, you can seamlessly interact with various APIs and harness the power of external data sources. By following the guidelines and examples outlined in this comprehensive guide, you are well on your way to becoming proficient in handling API requests in Python."
-    with open("test.md", "w") as f:
-        f.write(raw_markdown)
+    import os
+    import torch
+
+    # print torch gpu info
+    print("torch.cuda.is_available():", torch.cuda.is_available())
+
+    model_path = os.path.join(
+        os.path.dirname(__file__),
+        "..",
+        "models",
+        "juggernautXL_v7Rundiffusion.safetensors",
+    )
+
+    from diffusers import StableDiffusionXLPipeline
+
+    pipeline = StableDiffusionXLPipeline.from_single_file(
+        model_path,
+        torch_dtype=torch.float16,
+        variant="fp16",
+    ).to("cuda")
+    pipeline.enable_xformers_memory_efficient_attention()
+
+    image = pipeline("test image").images[0]
+
+    image.save("test.png")
+
 
 if __name__ == "__main__":
     main()
