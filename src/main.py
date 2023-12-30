8874584d9fe7edcb4bf4cde3e71512f98c4e11b8
Ryland Birchmeier <rylandbirchmeier@gmail.com>
2023-12-29 19:26:48 -0600

    reinit apis on failure

--- src/main.py
+++ src/main.py
@@ -7,12 +7,70 @@ from datetime import datetime
 
 
 def main():
+    fetch_api, llm_idea_generator, llm_writer, gen, upload_api = initialize_apis()
+
+    while True:
+        try:
+            # Fetch Categories
+            all_categories = fetch_api.fetch_categories()
+            category_constraint = [x.title for x in all_categories]
+
+            # Generate Random Article Ideas
+            kill_vram_processes()
+            article_idea = llm_idea_generator.generate_random_article_description()
+            print(f"Article Idea: {article_idea}\n\n")
+
+            # Generate Article For That Idea
+            article = llm_writer.write_news_article(
+                article_description=article_idea,
+                category_constraint=category_constraint,
+            )
+            print(f"Title: {article['title']}")
+            print(f"Category List: {article['category_list']}")
+            print(f"Image Description: {article['header_img_description']}")
+            print(f"Body Start: {article['body']}")
+
+            # Generate Image
+            kill_vram_processes()
+            img = gen.generate_image(article["header_img_description"])
+
+            # Publish Article
+            uploaded_asset = upload_api.upload_asset(img)
+            print(f"Uploaded Asset: {uploaded_asset}")
+
+            uploaded_article = upload_api.upload_news_article(
+                title=article["title"],
+                content=article["body"],
+                publishedDate=datetime.now(),
+                featuredImage=uploaded_asset,
+                categories=[
+                    x for x in all_categories if x.title in article["category_list"]
+                ],
+            )
+            print(f"Uploaded Article: {uploaded_article}")
+        except Exception as e:
+            print(str(e))
+            (
+                fetch_api,
+                llm_idea_generator,
+                llm_writer,
+                gen,
+                upload_api,
+            ) = initialize_apis()
+
+
+def initialize_apis():
     fetch_api = ContentfulFetchAPI(
         management_api_token=CONTENTFUL_MANAGEMENT_API_TOKEN,
         space_id=CONTENTFUL_SPACE_ID,
         environment_id=CONTENTFUL_ENVIRONMENT_ID,
     )
-    llm = OllamaInOut(model_name=OLLAMA_MODEL, temperature=OLLAMA_TEMPERATURE)
+    llm_idea_generator = OllamaInOut(
+        model_name=OLLAMA_MODEL, temperature=OLLAMA_TEMPERATURE_IDEA_GENERATOR
+    )
+    llm_writer = OllamaInOut(
+        model_name=OLLAMA_MODEL, temperature=OLLAMA_TEMPERATURE_WRITER
+    )
     gen = DiffusersTextToImage(
         pretrained_model_name_or_path=HUGGINGFACE_DIFFUSERS_PRETRAINED_MODEL_NAME_OR_PATH
     )
@@ -21,44 +79,7 @@ def main():
         space_id=CONTENTFUL_SPACE_ID,
         environment_id=CONTENTFUL_ENVIRONMENT_ID,
     )
-    while True:
-        # Fetch Categories
-        all_categories = fetch_api.fetch_categories()
-        category_constraint = [x.title for x in all_categories]
-
-        # Generate Random Article Ideas
-        kill_vram_processes()
-        article_idea = llm.generate_random_article_description()
-        print(f"Article Idea: {article_idea}\n\n")
-
-        # Generate Article For That Idea
-        article = llm.write_news_article(
-            article_description=article_idea,
-            category_constraint=category_constraint,
-        )
-        print(f"Title: {article['title']}")
-        print(f"Category List: {article['category_list']}")
-        print(f"Image Description: {article['header_img_description']}")
-        print(f"Body Start: {article['body']}")
-
-        # Generate Image
-        kill_vram_processes()
-        img = gen.generate_image(article["header_img_description"])
-
-        # Publish Article
-        uploaded_asset = upload_api.upload_asset(img)
-        print(f"Uploaded Asset: {uploaded_asset}")
-
-        uploaded_article = upload_api.upload_news_article(
-            title=article["title"],
-            content=article["body"],
-            publishedDate=datetime.now(),
-            featuredImage=uploaded_asset,
-            categories=[
-                x for x in all_categories if x.title in article["category_list"]
-            ],
-        )
-        print(f"Uploaded Article: {uploaded_article}")
+    return fetch_api, llm_idea_generator, llm_writer, gen, upload_api
 
 
 def kill_vram_processes() -> None:
